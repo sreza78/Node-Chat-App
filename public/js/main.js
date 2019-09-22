0d67b6aed243566cf4725f842ab64a081cb84030
seyed reaza <rezasefidgar78@gmail.com>
2019-09-22 23:05:59 +0330

    Version 2.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
     var socket = io.connect();
-    var messageForm = $("#messageForm");
-    var message = $("#message");
-    var chat = $("#chat");
-    var messageArea = $("#messageArea");
+    var me;
+    var notification = new Audio("../sounds/ring.mp3");
+
     var userFormArea = $("#userFormArea");
     var userForm = $("#userForm");
-    var users = $("#users");
     var username = $("#username");
-    var me;
+
+    var messageArea = $("#messageArea");
+    var users = $("#users");
+    var messageForm = $(".messages");
+    var text = $("#text");
+    var sendmsg = $(".send");
+    var image = $("#image");
+    var time = $("#time")
 
     userForm.submit(function(e) {
         e.preventDefault();
         socket.emit("new user", username.val(), function(data) {
             if (data) {
                 userFormArea.hide();
-                messageArea.show();
+                messageArea.fadeIn(500)
             }
         });
         me = username.val();
@@ -30,27 +35,92 @@ $(document).ready(function() {
         }
     })*/
 
-    messageForm.submit(function(e) {
+    sendmsg.click(function(e) {
         e.preventDefault();
-        if(message.val() != "")
-            socket.emit("send message", message.val());
-        message.val("")
+        if (text.val() != "")
+            socket.emit("send message", text.val());
+        text.val("")
+        notification.currentTime = 0;
+        text.focus();
     })
 
     socket.on("new message", function(data) {
         if (data.user == me) {
-            chat.append('<div class="chatbox"><strong>' + data.user + '</strong><p>' + data.msg + '</p></div>')
+            messageForm.append('<li class="i"><div class="message"><div class="head"><span class="name">' + data.user +
+                '</span></div><div class="txt">' + data.msg + '</div><div class="time">' + data.time + '</div></div></li>')
         } else {
-            chat.append('<div class="chatbox other"><strong>' + data.user + '</strong><p>' + data.msg + '</p></div>')
+            messageForm.append('<li class="friend-with-a-SVAGina"><div class="message"><div class="head"><span class="name">' + data.user +
+                '</span></div><div class="txt">' + data.msg + '</div><div class="time">' + data.time + '</div></div></li>')
+            notification.play();
         }
-        chat.animate({ scrollTop: chat.prop("scrollHeight") }, 500);
+        messageForm.animate({ scrollTop: messageForm.prop("scrollHeight") }, 500);
     })
+
+    image.change(function(e) {
+        e.preventDefault();
+        if (image.val().length) {
+            var fReader = new FileReader();
+            fReader.readAsDataURL(e.target.files[0]);
+            fReader.onloadend = function(event) {
+
+                socket.emit("send image", event.target.result);
+                image.val("");
+                notification.currentTime = 0;
+            }
+        }
+    })
+
+
+    socket.on("new image", function(data) {
+        if (data.image) {
+            var img = new Image();
+            img.src = data.buffer;
+            if (data.user == me) {
+                messageForm.append('<li class="i"><div class="message"><div class="head"><span class="name">' + data.user +
+                    '</span></div><img src="' + img.src + '" class="img-thumbnail"><div class="time">' + data.time + '</div></div></li>')
+            } else {
+                messageForm.append('<li class="friend-with-a-SVAGina"><div class="message"><div class="head"><span class="name">' + data.user +
+                    '</span></div><img src="' + img.src + '" class="img-thumbnail"><div class="time">' + data.time + '</div></div></li>')
+                notification.play();
+            }
+            messageForm.animate({ scrollTop: messageForm.prop("scrollHeight") }, 500);
+        }
+    });
 
     socket.on('get users', function(data) {
         var html = "";
         for (i = 0; i < data.length; i++) {
-            html += "<li class='list-group-item'>" + data[i] + "</li>"
+            if (data[i].username == me) {
+                html += '<li><div class="info"><div class="user">' + data[i].username + '</div><div class="status off">' + data[i].ip + '</div>' +
+                    '</div></li>'
+            } else {
+                html += '<li><div class="info"><div class="user">' + data[i].username + '</div><div class="status on">' + data[i].ip + '</div>' +
+                    '</div></li>'
+            }
         }
         users.html(html);
     })
+
+
+
+    conf = {
+        cursorcolor: "#696c75",
+        cursorwidth: "4px",
+        cursorborder: "none"
+    };
+
+    lol = {
+        cursorcolor: "#cdd2d6",
+        cursorwidth: "4px",
+        cursorborder: "none"
+    };
+
+    claerResizeScroll = function() {
+        $("#text").val("");
+        $(".messages").getNiceScroll(0).resize();
+        return $(".messages").getNiceScroll(0).doScrollTop(999999, 999);
+    };
+    $(".list-friends").niceScroll(conf);
+    $(".messages").niceScroll(lol);
+
 })
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 var app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const fs = require('fs')
+const moment = require('moment');
+const requestIp = require('request-ip');
 users = [];
 connections = [];
 
@@ -10,10 +13,17 @@ console.log('server is running at port 2020');
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+app.use(requestIp.mw())
+ 
+app.use(function(req, res) {
+    const ip = req.clientIp;
+    res.end(ip);
+});
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 })
+
 
 io.sockets.on('connection', function(socket) {
     connections.push(socket);
@@ -28,17 +38,25 @@ io.sockets.on('connection', function(socket) {
     })
 
     socket.on('send message', function(data) {
-        io.sockets.emit("new message", { msg: data, user: socket.username })
+        io.sockets.emit("new message", { msg: data, user: socket.username, time: moment().format("hh:mm a") })
+    })
+
+    socket.on('send image', function(data) {
+        io.sockets.emit('new image', { image: true, buffer: data, user:socket.username , time: moment().format("hh:mm a")});
     })
 
     socket.on("new user", function(data, callback) {
         callback(true);
         socket.username = data;
-        users.push(socket.username);
+        user ={
+            ip : socket.handshake.address.split(':')[3],
+            username: socket.username
+        }
+        users.push(user);
         UpdateUsername();
     })
 
-    function UpdateUsername(){
-    	io.sockets.emit("get users", users)
+    function UpdateUsername() {
+        io.sockets.emit("get users", users)
     }
 })
